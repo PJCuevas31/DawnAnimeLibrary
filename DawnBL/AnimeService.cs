31c20b22bfed152a9955f7b2bf3fab3aa6214ecc
admin <admin@DESKTOP-POSRD7G>
2024-04-20 21:26:18 +0800

    Add project files.

---
+++ DawnBL/AnimeService.cs
@@ -0,0 +1,26 @@
+﻿using DawnDL;
+using DawnModel;
+using System;
+using System.Collections.Generic;
+using System.Xml.Linq;
+
+namespace DawnBL;
+
+public class AnimeService
+{
+
+    public List<Anime> AniRep = new List<Anime>();
+    public List<Anime> GetAnimes()
+    {
+        AnimeRepository data = new AnimeRepository();
+        return data.GetAnimes();
+    }
+    public void AddAnime(Anime animex)
+    {
+        AniRep.Add(animex);
+    }
+    public void DeleteAnime(Anime animex)
+    {
+        AniRep.Remove(animex);
+    }
+}
---
+++ DawnModel/Anime.cs
@@ -0,0 +1,10 @@
+﻿namespace DawnModel
+{
+
+    public class Anime
+    {
+        public string? aniname;
+        public string? anigenre;
+        public string? anipublisher;
+    }
+}
